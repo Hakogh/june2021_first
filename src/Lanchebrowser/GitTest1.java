90d9e251876259e0de82a3a32c88a2c20f6c0933
Hakogh <41879703+Hakogh@users.noreply.github.com>
2021-11-16 23:46:19 -0600

    4th commit

--- src/Lanchebrowser/GitTest1.java
+++ src/Lanchebrowser/GitTest1.java
@@ -2,4 +2,9 @@ package Lanchebrowser;
 
 public class GitTest1 {
 
+	public static void main(String[] args) {
+		System.out.println("1");
+		System.out.println("2");
+		System.out.println("3");
+	}
 }
